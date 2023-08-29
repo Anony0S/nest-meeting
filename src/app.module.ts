2b7c1526da7c81c08d043a3bc9712c233b6a3adb
Anony0S <ljx1583861@qq.com>
2023-08-29 10:37:13 +0800

    上传Github

---
+++ src/app.module.ts
@@ -0,0 +1,57 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+import { Role } from './user/entities/role.entity';
+import { User } from './user/entities/user.entity';
+import { Permission } from './user/entities/permission.entity';
+import { RedisModule } from './redis/redis.module';
+import { EmailModule } from './email/email.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory(configService: ConfigService) {
+        return {
+          type: 'mysql',
+          host: configService.get('mysql_server_host'),
+          port: configService.get('mysql_server_port'),
+          username: configService.get('mysql_server_username'),
+          password: configService.get('mysql_server_password'),
+          database: configService.get('mysql_server_database'),
+          synchronize: true,
+          logging: true,
+          entities: [User, Role, Permission],
+          poolSize: 10,
+          connectorPackage: 'mysql2',
+        };
+      },
+      inject: [ConfigService],
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `${__dirname}/.env`,
+    }),
+    JwtModule.registerAsync({
+      global: true,
+      useFactory(configService: ConfigService) {
+        return {
+          secret: configService.get('jwt_secret'),
+          signOptions: {
+            expiresIn: '30m',
+          },
+        };
+      },
+      inject: [ConfigService],
+    }),
+    UserModule,
+    RedisModule,
+    EmailModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/user/user.controller.ts
@@ -0,0 +1,107 @@
+import { Body, Controller, Get, Inject, Post, Query } from '@nestjs/common';
+import { UserService } from './user.service';
+import { RegisterUserDto } from './dto/register-user.dto';
+import { RedisService } from 'src/redis/redis.service';
+import { EmailService } from 'src/email/email.service';
+import { LoginUserDto } from './dto/login-user.dto';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+
+@Controller('user')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Inject(RedisService)
+  private redisService: RedisService;
+
+  @Inject(EmailService)
+  private emailService: EmailService;
+
+  @Inject(JwtService)
+  private jwtService: JwtService;
+
+  @Inject(ConfigService)
+  private configService: ConfigService;
+
+  // 注册
+  @Post('register')
+  async register(@Body() registerUser: RegisterUserDto) {
+    const res = await this.userService.register(registerUser);
+
+    return {
+      statusCode: 200,
+      message: res,
+    };
+  }
+
+  // 获取验证码
+  @Get('register-captcha')
+  async captcha(@Query('address') address: string) {
+    const code = Math.random().toString().slice(2, 8);
+    await this.redisService.set(`captcha_${address}`, code, 5 * 60);
+    await this.emailService.sendMail({
+      to: address,
+      subject: '注册验证码',
+      html: `<p>您的验证码是：${code}</p>`,
+    });
+    return {
+      statusCode: 200,
+      message: '发送成功！',
+    };
+  }
+
+  // 初始化
+  @Get('init-data')
+  async init() {
+    await this.userService.initData();
+    return {
+      statusCode: 200,
+      message: '初始化完成',
+    };
+  }
+
+  // 登录 - 普通用户
+  @Post('login')
+  async userLogin(@Body() loginUser: LoginUserDto) {
+    const vo = await this.userService.login(loginUser, false);
+
+    vo.accessToken = this.jwtService.sign(
+      {
+        userId: vo.userInfo.id,
+        username: vo.userInfo.username,
+        roles: vo.userInfo.roles,
+        permissions: vo.userInfo.permissions,
+      },
+      {
+        expiresIn:
+          this.configService.get('jwt_access_token_expires_time') || '30m',
+      },
+    );
+
+    vo.refreshToken = this.jwtService.sign(
+      {
+        userId: vo.userInfo.id,
+      },
+      {
+        expiresIn:
+          this.configService.get('jwt_refresh_token_expres_time') || '7d',
+      },
+    );
+    return {
+      statusCode: 200,
+      message: '登录成功',
+      userInfo: vo,
+    };
+  }
+
+  // 登录 - 管理员
+  @Post('admin/login')
+  async adminLogin(@Body() adminUser: LoginUserDto) {
+    const vo = await this.userService.login(adminUser, true);
+    return {
+      statusCode: 200,
+      message: '登录成功',
+      userInfo: vo,
+    };
+  }
+}
